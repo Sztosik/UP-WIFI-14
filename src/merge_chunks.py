894ea948d91d1419429fd7abffcadcd57a6add1f
Jakub <jakubmisiurka@gmail.com>
2023-11-16 14:25:23 +0100

    added merge_chunks

---
+++ src/merge_chunks.py
@@ -0,0 +1,9 @@
+def merge_chunks(chunk_list, output_file):
+    file_data = bytearray()
+    for chunk in chunk_list:
+        for byte in chunk:
+            file_data.append(byte)
+
+    file = open(f"{output_file}.txt", "wb")
+    file.write(file_data)
+    file.close()
