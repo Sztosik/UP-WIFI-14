cff8d51f04083c4b561aa5973bb06886918e5170
Jędrzej Stasik <jedrzej.stasik@gmail.com>
2023-11-16 12:11:15 +0100

    fix: formatting

--- src/udp_receiver.py
+++ src/udp_receiver.py
@@ -18,9 +18,9 @@ class ReceiverTask(Task):
             print("received message: %s" % str(data))
 
 
-#sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Internet  # UDP
-#sock.bind((UDP_IP, UDP_PORT))
+# sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Internet  # UDP
+# sock.bind((UDP_IP, UDP_PORT))
 
-#while True:
-    #data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
-    #print("received message: %s" % str(data))
+# while True:
+# data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
+# print("received message: %s" % str(data))
--- src/udp_sender.py
+++ src/udp_sender.py
@@ -4,29 +4,22 @@ from task import Task
 
 UDP_IP = "192.168.1.101"
 UDP_PORT = 5005
-MESSAGE = b"Siemano"
 
 
 class SenderTask(Task):
     def __init__(self):
         super().__init__()
         print("UDP target IP: %s" % UDP_IP)
-        print("UDP target port: %s" % UDP_PORT)
-        print("message: %s" % MESSAGE)
+        print("UDP target port: %s \n" % UDP_PORT)
+
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Internet  # UDP
 
     def _run(self) -> None:
         while True:
-            msg = bytes(input("Wpisz wiadomość: "))
+            msg = bytes(input("Wpisz wiadomość: "), "utf-8")
             self.sock.sendto(msg, (UDP_IP, UDP_PORT))
+
 
 if __name__ == "__main__":
     sender_task = SenderTask()
     sender_task.start()
-
-#print("UDP target IP: %s" % UDP_IP)
-#print("UDP target port: %s" % UDP_PORT)
-#print("message: %s" % MESSAGE)
-
-#sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Internet  # UDP
-#sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
