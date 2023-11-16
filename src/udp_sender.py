5cc133eb55636cd855ca1fdd8254df50e3d2428e
Jakub <jakubmisiurka@gmail.com>
2023-11-16 13:03:51 +0100

    sender

--- src/udp_sender.py
+++ src/udp_sender.py
@@ -10,16 +10,23 @@ MESSAGE = b"Siemano"
 class SenderTask(Task):
     def __init__(self):
         super().__init__()
+        print("UDP target IP: %s" % UDP_IP)
+        print("UDP target port: %s" % UDP_PORT)
+        print("message: %s" % MESSAGE)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Internet  # UDP
 
     def _run(self) -> None:
         while True:
-            # write your code here
-            pass
+            msg = bytes(input("Wpisz wiadomość: "))
+            self.sock.sendto(msg, (UDP_IP, UDP_PORT))
 
+if __name__ == "__main__":
+    sender_task = SenderTask()
+    sender_task.start()
 
-print("UDP target IP: %s" % UDP_IP)
-print("UDP target port: %s" % UDP_PORT)
-print("message: %s" % MESSAGE)
+#print("UDP target IP: %s" % UDP_IP)
+#print("UDP target port: %s" % UDP_PORT)
+#print("message: %s" % MESSAGE)
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Internet  # UDP
-sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+#sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Internet  # UDP
+#sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
