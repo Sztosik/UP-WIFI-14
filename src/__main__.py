4a1ed50027d494b9bc2e8e2dead91d70d313035b
Jakub <jakubmisiurka@gmail.com>
2023-11-16 13:22:41 +0100

    target ip prompt

--- src/__main__.py
+++ src/__main__.py
@@ -1,8 +1,10 @@
 from .udp_receiver import ReceiverTask
 from .udp_sender import SenderTask
 
+target_ip = input("Podaj docelowe IP: ")
+
 receiver_task = ReceiverTask()
 receiver_task.start()
 
-sender_task = SenderTask()
+sender_task = SenderTask( target_ip)
 sender_task.start()
--- src/udp_sender.py
+++ src/udp_sender.py
@@ -2,14 +2,13 @@ import socket
 
 from .task import Task
 
-UDP_IP = "192.168.1.101"
 UDP_PORT = 5005
 
-
 class SenderTask(Task):
-    def __init__(self):
+    def __init__(self, target_ip):
         super().__init__()
-        print("UDP target IP: %s" % UDP_IP)
+        self.target_ip = target_ip
+        print("UDP target IP: %s" % target_ip)
         print("UDP target port: %s \n" % UDP_PORT)
 
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Internet  # UDP
@@ -17,7 +16,7 @@ class SenderTask(Task):
     def _run(self) -> None:
         while True:
             msg = bytes(input("Wpisz wiadomość: "), "utf-8")
-            self.sock.sendto(msg, (UDP_IP, UDP_PORT))
+            self.sock.sendto(msg, (self.target_ip, UDP_PORT))
 
 
 if __name__ == "__main__":
