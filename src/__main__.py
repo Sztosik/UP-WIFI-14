0d6218556e1a3b5e1aab755e142dbc52a963384d
Jakub <jakubmisiurka@gmail.com>
2023-11-16 13:14:54 +0100

    added sender task to main

--- src/__main__.py
+++ src/__main__.py
@@ -1,4 +1,8 @@
 from .udp_receiver import ReceiverTask
+from .udp_sender import SenderTask
 
 receiver_task = ReceiverTask()
 receiver_task.start()
+
+sender_task = SenderTask()
+sender_task.start()
