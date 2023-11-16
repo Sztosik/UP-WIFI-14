1da4b6981a699f6254e8ffe08d74c4fee08fe0bd
Jędrzej Stasik <jedrzej.stasik@gmail.com>
2023-11-16 11:58:57 +0100

    fuszera hotfix

--- src/udp_receiver.py
+++ src/udp_receiver.py
@@ -1,6 +1,6 @@
 import socket
 
-from task import Task
+from .task import Task
 
 UDP_IP = "192.168.1.101"
 UDP_PORT = 5005
@@ -9,12 +9,12 @@ UDP_PORT = 5005
 class ReceiverTask(Task):
     def __init__(self):
         super().__init__()
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Internet  # UDP
-        sock.bind((UDP_IP, UDP_PORT))
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Internet  # UDP
+        self.sock.bind((UDP_IP, UDP_PORT))
 
     def _run(self) -> None:
         while True:
-            data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
+            data, addr = self.sock.recvfrom(1024)  # buffer size is 1024 bytes
             print("received message: %s" % str(data))
 
 
